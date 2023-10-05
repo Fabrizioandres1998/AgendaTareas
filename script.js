7cde1d50de269ded2055a142a05ac49ffb0345ca
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-05 15:47:48 -0300

    asd

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 const input = document.getElementById("tareaInput");
 const lista = document.getElementById("listaTareas");
 let array = [];
+let estado = "Sin completar"
 
-input.addEventListener("keydown", function(event) {
+input.addEventListener("keydown", function (event) {
     if (event.code === "Enter") {
         let tarea = input.value;
         const li = document.createElement('li');
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        const textoTarea = document.createTextNode(tarea);
+        const textoTarea = document.createTextNode(tarea)
+        array.push({ "Tarea: ": tarea, "Estado: ": estado });
 
         li.appendChild(checkbox);
         li.appendChild(textoTarea);
         lista.appendChild(li);
-
-        checkbox.addEventListener("change", function() {
-            let estado = checkbox.checked ? "Terminada" : "No terminada";
-            array.push({ "Tarea: ": tarea, "Estado": estado });
-            console.log(array);
-        });
-
+        
         input.value = "";
+        console.log(array)
+
+        checkbox.addEventListener("change", function () {
+            if (checkbox.checked) {
+                estado = "Completada"
+            }
+        })
     }
+
 });
-console.log(array)
+
+
+
+// checkbox.addEventListener("change", function() {
+// });
