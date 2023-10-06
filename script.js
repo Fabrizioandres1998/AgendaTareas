e82774086f15e7f9db18da8b4e0552695dae0f56
Fabrizio D'Isidoro <fabrizioandres98@gmail.com>
2023-10-05 23:52:30 -0300

    feat(gral): codigo funcionando, falta agregar alguna forma de poder eliminar las tareas y que salga el estado de la misma al lado

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const input = document.getElementById("tareaInput");
-const lista = document.getElementById("listaTareas");
-let array = [];
-let estado = "Sin completar"
+const input = document.getElementById("tareaInput")
+const lista = document.getElementById("listaTareas")
+let array = []
 
 input.addEventListener("keydown", function (event) {
     if (event.code === "Enter") {
-        let tarea = input.value;
-        const li = document.createElement('li');
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
+        let tarea = input.value
+        let estado = "Sin terminar"
+        let objeto = { "Tarea: ": tarea, "Estado: ": estado }
+        const li = document.createElement('li')
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
         const textoTarea = document.createTextNode(tarea)
-        array.push({ "Tarea: ": tarea, "Estado: ": estado });
-
-        li.appendChild(checkbox);
-        li.appendChild(textoTarea);
-        lista.appendChild(li);
-        
-        input.value = "";
-        console.log(array)
 
         checkbox.addEventListener("change", function () {
             if (checkbox.checked) {
-                estado = "Completada"
+                objeto["Estado: "] = "Terminado"
+            } else {
+                objeto["Estado: "] = "Sin terminar"
             }
+            array[index] = objeto
+            console.log(array)
         })
+
+        const index = array.push(objeto) - 1
+
+        li.appendChild(checkbox)
+        li.appendChild(textoTarea)
+        lista.appendChild(li)
+
+        input.value = ""
+        console.log(array)
     }
-
-});
-
-
-
-// checkbox.addEventListener("change", function() {
-// });
+})
